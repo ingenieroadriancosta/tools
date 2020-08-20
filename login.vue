2c47c314a305896aa2a1a56ccc70cf4da19cecaf
Adrian Costa <7077791+ingenieroadriancosta@users.noreply.github.com>
2020-08-20 12:23:07 -0500

    Add files via upload

--- login.vue
+++ login.vue
@@ -1,3 +1,4 @@
+<!-- ADRIAN COSTA -->
 <template>
   <v-container>
       
@@ -22,14 +23,50 @@
       <v-divider vertical></v-divider>
     </v-toolbar-items>
         </v-app-bar>
-
-
-    
-      <div class="mt-1" v-show="!logshow">
-          <h2>Principal<v-icon  right>mdi-home</v-icon></h2>
-          
-      </div>
+        <div class="mt-1" v-show="!logshow">
+            <h2>Principal<v-icon  right>mdi-home</v-icon></h2>
+            <v-toolbar
+                    dark
+                    color="teal"
+                >
+                <v-text-field
+                v-model="selecttext"
+                @keyup="tableord"
+                cache-items
+                class="mx-4"
+                flat
+                hide-no-data
+                hide-details
+                label="Presione ENTER para Buscar un email"
+                solo-inverted
+                ></v-text-field>
+            </v-toolbar>
+            <!-- -->
+            <!-- -->
+            <!-- -->
+            <!-- -->
+            <v-data-table
+                :headers="headers"
+                :items="desserts"
+                :items-per-page="5"
+                class="elevation-1"
+            ></v-data-table>
+            <!-- -->
+            <!-- -->
+            <!-- -->
+            <!-- -->
+        </div>
       
+
+
+
+
+
+
+
+
+
+
       <div class="mt-1" v-show="logshow">
           <h2>Login</h2>
         <v-col cols="7" sm="6" md="3" id="email">
@@ -95,16 +132,48 @@
         loginexitoso:false,
         datin:Date.now(),
         datout:Date.now(),
+        inter:[],
         fpost: {
             email:"eve.holt@reqres.in",
             password:"pistol",
         },
-        
         emailRules: [
                 v => !!v || "E-mail is required",
                 v => /.+@.+/.test(v) || "E-mail must be valid"
             ],
         loginRules: [v => !!v || "The input is required"],
+
+        selecttext:"",
+
+        headers: [
+          {
+            text: 'Id',
+            align: 'start',
+            sortable: true,
+            value: 'id',
+          },
+          { text: 'email', value: 'email',sortable: false, },
+          { text: 'First name', value: 'first_name',sortable: false, },
+          { text: 'Last name', value: 'last_name',sortable: false, },
+          { text: 'Avatar', value: 'avatar',sortable: false, },
+        ],
+        desserts: [
+          {
+            id: 1,
+            email: 159,
+            first_name: 6.0,
+            last_name: 24,
+          },
+          {
+            id: 2,
+            email: 237,
+            first_name: 9.0,
+            last_name: 37,
+          },
+        ],
+        dessertsc: [],
+        emailmatch:""
+        
       }
     },
     
@@ -118,9 +187,9 @@
       },
       clilog: function () {
           this.logtit="Login"
+          this.logshow=true
           if( this.loginexitoso ){
               this.loginexitoso=false
-              this.logshow=true
               this.datout=new Date(Date.now())
               const millis = (this.datout - this.datin)/1000;
               this.infomsg("LogOut", "Gracias [" + 
@@ -155,8 +224,7 @@
           this.msgtext = msg
       },
       clsmsg: function (event) {
-          this.dialoginfo=false
-          this.dialogerror=false
+          this.dialoginfo = false;
       },
       ntimfnc: function (event) {
           this.ntimesfailed++;
@@ -170,9 +238,86 @@
           this.nsegs++;
           this.msgtext = "Error al ingresar, espere, "+this.nsegs+" segundos de 300"
           if( this.nsegs>=300 ){
-              clsmsg()
+              clearInterval(this.inter)
+              this.clsmsg()
           }
       },
+
+
+        tableord:function(rejson){
+            var dat = rejson['data'];
+            this.desserts=[]
+            this.dessertsc.forEach(element => {
+                if( element['email'].includes(this.selecttext) ){
+                    this.desserts.push(element)
+                }
+            });
+        },
+
+
+        tableresponse:function(rejson){
+            var dat = rejson['data'];
+            this.desserts=[]
+            this.dessertsc=[]
+            dat.forEach(element => {
+                this.desserts.push(element)
+                this.dessertsc.push(element)
+            });
+        },
+
+
+
+        filltable:function(){
+            var tbres = this.tableresponse;
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "https://reqres.in/api/users?page=1", true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(null);
+            xhr.onreadystatechange = function () {
+                if(this.readyState!=4){
+                    return;
+                }
+                if(this.status==200){
+                    tbres( JSON.parse(this.responseText) );
+                }else{
+                    tbres( null );
+                }
+                return;
+            };
+        },
+
+
+
+
+
+
+
+                /*
+        headers: [
+          {
+            text: 'Id',
+            align: 'start',
+            sortable: true,
+            value: 'id',
+          },
+          { text: 'email', value: 'email',sortable: false, },
+          { text: 'First name', value: 'first_name',sortable: false, },
+          { text: 'Last name', value: 'last_name',sortable: false, },
+          { text: 'Avatar', value: 'avatar',sortable: false, },
+        ],
+        desserts: [
+            }
+            //*/
+            
+
+
+
+
+
+
+
+
+
       onreadystatechange: function (readyState, status, responseText) {
           if (readyState != 4){
                 return;
@@ -190,7 +335,7 @@
                 if(this.ntimesfailed>=3){
                     this.bloqb=true
                     this.nsegs = 0
-                    setInterval(this.timero, 1000);
+                    this.inter = setInterval(this.timero, 1000);
                 }
                 return;
             }
@@ -206,6 +351,7 @@
                     this.fpost.email+"<br/>"+
                     " Se ha logueado con éxito a la plataforma"
                     )
+                this.filltable();
             }
       },
       // 
